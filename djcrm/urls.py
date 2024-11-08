e5b5ac354ced5771478536612134b9d91090ed13
npxpatel <nirajkumarpatel21@gmail.com>
2024-11-09 02:22:57 +0530

    Added userprofile and loginMixin

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', LandingPageView.as_view()),
+    path('', LandingPageView.as_view(), name='landing-page'),
     path('leads/', include('leads.urls', namespace='leads')),
     path('login/', LoginView.as_view(), name = 'login'),
     path('logout/', LogoutView.as_view(), name='logout'),
--- leads/models.py
+++ leads/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
+from django.db.models.signals import post_save
+#an organistion having multiple agents who can have mulitple leads:
 
 class User(AbstractUser):
     pass 
 
+class UserProfile(models.Model):
+    # it doesn't get created automatically after the User is created, use signals bro
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username}"
+
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    origanisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     
     def __str__(self):
         return f"{self.user.email}"
@@ -33,3 +41,9 @@ class Lead(models.Model):
     def __str__(self):
         return f"{self.first_name} by {self.source}"
     
+
+def post_user_created_signal(sender, instance, created, **kwargs):
+     if created:
+         UserProfile.objects.create(user=instance)
+
+post_save.connect(post_user_created_signal, sender=User)      #model that sends the event to create a userprofile after a newUser joins
